9106a06f803b8351ef332d7897c2f4c371f23876
Daniel Wickman & Kaley La Porta & Mumo Musau <pair+dwwickman.klaporta415.mmusau99@devbootcamp.com>
2017-05-31 15:20:34 -0500

    Migrations, models, testing directory

---
+++ db/migrate/20170531195612_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :director
+      t.date :release_date
+      t.string :poster_url
+      t.string :background_url
+      t.string :trailer_url
+
+      t.timestamps null: false
+    end
+  end
+end
