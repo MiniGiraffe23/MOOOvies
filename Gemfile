6d8c34ee98613ead566149e9c62ddfb2d0e8b356
Daniel Wickman & Kaley La Porta & Mumo Musau <pair+dwwickman.klaporta415.mmusau99@devbootcamp.com>
2017-05-31 18:22:34 -0500

    movie objects without director and trailer

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'coffee-rails', '~> 4.2'
 # gem 'therubyracer', platforms: :ruby
 gem 'devise'
 
-
+gem 'dotenv-rails', groups: [:development, :test]
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,112 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'uri'
+require 'pathname'
+require 'net/http'
+require 'json'
+require 'ostruct'
+
+  key = ENV["TMD_API_KEY"]
+
+
+  url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&api_key=" + key
+
+  uri = URI(url)
+
+
+  request = Net::HTTP.get(uri)
+
+  obj = JSON.parse(request)
+
+
+  obj["results"].each do |movie|
+
+    if movie["backdrop_path"] == nil
+      background = nil
+    else
+      background = "https://image.tmdb.org/t/p/w1440_and_h320_bestv2/" + movie["backdrop_path"]
+    end
+
+    if movie["poster_path"] == nil
+      poster = nil
+    else
+      poster = "https://image.tmdb.org/t/p/w640" + movie["poster_path"]
+    end
+   Movie.create!(tmdid: movie["id"], title: movie["title"],
+    poster_url: poster, background_url: background, release_date: movie["release_date"])
+  end
+
+
+
+
+
+
+
+
+
+
+  url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=2&api_key=" + key
+
+  uri = URI(url)
+
+
+  request = Net::HTTP.get(uri)
+
+  obj = JSON.parse(request)
+
+
+  obj["results"].each do |movie|
+
+    if movie["backdrop_path"] == nil
+      background = nil
+    else
+      background = "https://image.tmdb.org/t/p/w1440_and_h320_bestv2/" + movie["backdrop_path"]
+    end
+
+    if movie["poster_path"] == nil
+      poster = nil
+    else
+      poster = "https://image.tmdb.org/t/p/w640" + movie["poster_path"]
+    end
+   Movie.create!(tmdid: movie["id"], title: movie["title"],
+    poster_url: poster, background_url: background, release_date: movie["release_date"])
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+  url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=3&api_key=" + key
+
+  uri = URI(url)
+
+
+  request = Net::HTTP.get(uri)
+
+  obj = JSON.parse(request)
+
+
+  obj["results"].each do |movie|
+
+    if movie["backdrop_path"] == nil
+      background = nil
+    else
+      background = "https://image.tmdb.org/t/p/w1440_and_h320_bestv2/" + movie["backdrop_path"]
+    end
+
+    if movie["poster_path"] == nil
+      poster = nil
+    else
+      poster = "https://image.tmdb.org/t/p/w640" + movie["poster_path"]
+    end
+   Movie.create!(tmdid: movie["id"], title: movie["title"],
+    poster_url: poster, background_url: background, release_date: movie["release_date"])
+  end
+
+
+  Movie.all
