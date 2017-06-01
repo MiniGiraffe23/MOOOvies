362f45d2bd71cc0d215bd20c9a91bf174dc9252b
Brittany Baranowski & Mumo Musau <pair+minigiraffe23.mmusau99@devbootcamp.com>
2017-06-01 15:44:08 -0500

    Homepage now displays top 3 rated movies, updated contoroller and added virtual attribute average rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,13 @@
 class Movie < ActiveRecord::Base
+  # scope :top_three, ->{ order("average_rating DESC ").limit(3)}
 
   has_many :comments
   has_many :ratings
   has_many :favorites
   has_many :watchlists
 
+
+  def average_rating
+    rating = self.ratings.reduce(0) {|sum, rating| (sum + rating.spots)/self.ratings.count }
+  end
 end
