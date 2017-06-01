25489698c6547730344d3c2fbd0cb1b848030733
Daniel Wickman & Kaley La Porta <pair+dwwickman.klaporta415@devbootcamp.com>
2017-06-01 17:49:14 -0500

    User show page with user's firstname in header, link to profile in layout header

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   resources :search
+  resources :users
   root 'movies#index'
 end
