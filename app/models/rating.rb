ffe6c5b5b6192a35c0956c446dd1cacb510858e2
Daniel Wickman & Kaley La Porta <pair+dwwickman.klaporta415@devbootcamp.com>
2017-06-02 12:21:47 -0500

    Rating validatinos for range spots 0-5 and integer

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,4 +4,5 @@ class Rating < ActiveRecord::Base
   belongs_to :movie
 
   validates :user, uniqueness: { scope: :movie }
+  validates :spots, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
